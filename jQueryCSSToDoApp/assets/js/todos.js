7becc0e7bcfb8a3570d566a622d45bd95047d27a
ga_shova.niraula <shova.niraula@gmail.com>
2020-02-29 17:20:51 -0500

    to-do-list completed, example of implementation of jQuery, CSS

---
+++ jQueryCSSToDoApp/assets/js/todos.js
@@ -0,0 +1,19 @@
+//check off specific todos by clicking
+$("ul").on("click", "li" ,function() {
+  $(this).toggleClass("completed");
+});
+
+//click on x to delete to-do
+$("ul").on("click","span",function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+event.stopPropagation();
+});
+$("input[type='text']").keypress(function(event){
+   if(event.which === 13){
+       let newToDoText = $(this).val();
+       $(this).val("");
+       $("ul").append("<Li><span><i class='fa fa-trash'></i></span>" + newToDoText + "</Li>");
+   }
+});
