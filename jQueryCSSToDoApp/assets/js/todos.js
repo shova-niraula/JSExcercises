e694acc568abfd59ae85fa6bbc5c6d9b7dc19918
ga_shova.niraula <shova.niraula@gmail.com>
2020-02-29 21:18:06 -0500

    added toggle function in jQuery

--- jQueryCSSToDoApp/assets/js/todos.js
+++ jQueryCSSToDoApp/assets/js/todos.js
@@ -1,19 +1,23 @@
 //check off specific todos by clicking
-$("ul").on("click", "li" ,function() {
-  $(this).toggleClass("completed");
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
 });
 
 //click on x to delete to-do
-$("ul").on("click","span",function(event){
-    $(this).parent().fadeOut(500,function(){
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
-event.stopPropagation();
+    event.stopPropagation();
 });
-$("input[type='text']").keypress(function(event){
-   if(event.which === 13){
-       let newToDoText = $(this).val();
-       $(this).val("");
-       $("ul").append("<Li><span><i class='fa fa-trash'></i></span>" + newToDoText + "</Li>");
-   }
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        let newToDoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<Li><span><i class='fa fa-trash'></i></span>" + newToDoText + "</Li>");
+    }
+});
+
+$("#fa-plus").click(function () {
+    $("input[type='text']").fadeToggle();
 });
