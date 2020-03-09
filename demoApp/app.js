53f3d599896a3571c4d0570ff2148b74e82cc730
ga_shova.niraula <shova.niraula@gmail.com>
2020-03-08 21:23:22 -0400

    npm example packages

---
+++ demoApp/app.js
@@ -0,0 +1,11 @@
+let catMe = require("cat-me");
+console.log(catMe());
+
+let func = shova;
+console.log(func());
+
+function shova() {
+    console.log("Buda loves you");
+}
+let jokes= require("knock-knock-jokes");
+console.log(jokes());
