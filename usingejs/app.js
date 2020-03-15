316a3721fc1497e244a317dc8ee4c0a7f22f6a98
ga_shova.niraula <shova.niraula@gmail.com>
2020-03-14 21:59:06 -0400

    using ejs looping

--- usingejs/app.js
+++ usingejs/app.js
@@ -5,9 +5,23 @@ app.use(express.static("views"));
 app.get("/",function(req, res){
     res.render("index.ejs");
 });
-app.get("/:subpage",function(req,res){
- let subpage =req.params.subpage;
-res.render("subpagecalling.ejs", {subpage:subpage});
+// app.get("/:subpage",function(req,res){
+//  let subpage =req.params.subpage;
+// res.render("subpagecalling.ejs", {subpage:subpage});
+// });
+ app.get("/:subpage/:subsubpage",function(req,res){
+    let subpage= req.params.subpage;
+    let subsubpage=req.params.subsubpage;
+    res.render("subsubpage.ejs",{subpage:subpage,subsubpage:subsubpage});
+});
+app.get("/posts", function(res, res){
+    var posts =[
+        { title:"Updated SNHU News", author: "ISS office"},
+        { title:"New Corona News", author: "Office of SNHU Adminitration"},
+        { title:"Commencement 2020", author: "Graduation Team Office"},
+        { title:"Employment", author: "SNHU employment office"},
+    ];
+    res.render("posts.ejs", {posts:posts});
 });
 
 app.listen(3000,function(){
